d64e94f6fd6a957f0149f99e8d988c8bb518d4ee
shadman_shihab <shadmanshihab0@gmail.com>
2020-10-13 00:05:43 +0600

    media query added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function closeMenu(){
+    document.getElementById("navbar").style.height = "0%";
+    document.getElementById("menu").style.display = "block";
+}
+
+function openMenu(){
+    document.getElementById("navbar").style.height = "100%";
+    document.getElementById("menu").style.display = "none";
+}
+
